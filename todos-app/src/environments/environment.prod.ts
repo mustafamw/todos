278ab7b6073a9a55eddc1c5389b46a19a3eae4ff
Mustafa Mohamed <mo.mustafa3@gmail.com>
2019-10-07 01:13:57 +0100

    updated

--- todos-app/src/environments/environment.prod.ts
+++ todos-app/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Environment } from 'src/app/interfaces/environment';
 export const environment: Environment = {
   production: true,
   api: {
-    domain: 'http://hushtech.co.uk',
+    domain: 'http://hushtech.co.uk:3000',
     path: '/api/v1'
   }
 };
